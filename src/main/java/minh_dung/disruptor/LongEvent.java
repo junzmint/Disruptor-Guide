9c36c32fbe1e1c8f7fc3cdc61c0031b9ce255d13
junzmint <junzmint07112002@gmail.com>
2024-03-02 20:25:10 +0700

    Define the Event that will carry the data

---
+++ src/main/java/minh_dung/disruptor/LongEvent.java
@@ -0,0 +1,19 @@
+package minh_dung.disruptor;
+
+
+// Firstly we will define the Event that will carry the data and is common to all following examples
+public class LongEvent
+{
+    private long value;
+
+    public void set(long value)
+    {
+        this.value = value;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "LongEvent{" + "value=" + value + '}';
+    }
+}
---
+++ src/main/java/minh_dung/disruptor/LongEventHandler.java
@@ -0,0 +1,13 @@
+package minh_dung.disruptor;
+
+import com.lmax.disruptor.EventHandler;
+
+// Once we have the event defined, we need to create a consumer that will handle these events. As an example, we will create an EventHandler that will print the value out to the console.
+public class LongEventHandler implements EventHandler<LongEvent>
+{
+    @Override
+    public void onEvent(LongEvent event, long sequence, boolean endOfBatch)
+    {
+        System.out.println("Event: " + event);
+    }
+}
