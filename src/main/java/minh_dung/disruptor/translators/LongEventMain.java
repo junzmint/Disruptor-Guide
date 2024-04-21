08bf600480cc15f3b7dcf18e0e78c44ef79ed35c
junzmint <junzmint07112002@gmail.com>
2024-04-21 12:06:02 +0700

    Downgrade from JDK 21 -> 17

--- src/main/java/minh_dung/disruptor/translators/LongEventMain.java
+++ src/main/java/minh_dung/disruptor/translators/LongEventMain.java
@@ -2,18 +2,15 @@ package minh_dung.disruptor.translators;
 
 import com.lmax.disruptor.RingBuffer;
 import com.lmax.disruptor.dsl.Disruptor;
-//import com.lmax.disruptor.examples.longevent.LongEvent;
-//import com.lmax.disruptor.examples.longevent.LongEventFactory;
-//import com.lmax.disruptor.examples.longevent.LongEventHandler;
-import minh_dung.disruptor.event.*;
 import com.lmax.disruptor.util.DaemonThreadFactory;
+import minh_dung.disruptor.event.LongEvent;
+import minh_dung.disruptor.event.LongEventFactory;
+import minh_dung.disruptor.event.LongEventHandler;
 
 import java.nio.ByteBuffer;
 
-public class LongEventMain
-{
-    public static void main(String[] args) throws Exception
-    {
+public class LongEventMain {
+    public static void main(String[] args) throws Exception {
         LongEventFactory factory = new LongEventFactory();
 
         int bufferSize = 1024;
@@ -25,8 +22,7 @@ public class LongEventMain
         RingBuffer<LongEvent> ringBuffer = disruptor.getRingBuffer();
         LongEventProducer producer = new LongEventProducer(ringBuffer);
         ByteBuffer bb = ByteBuffer.allocate(8);
-        for (long l = 0; true; l++)
-        {
+        for (long l = 0; true; l++) {
             bb.putLong(0, l);
             producer.onData(bb);
             Thread.sleep(1000);
